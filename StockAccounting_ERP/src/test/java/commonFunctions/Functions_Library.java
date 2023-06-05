f86253af3275cdc6070ed5ef26efd56d328bfd98
MAX HUB <MAX HUB@DESKTOP-4M14A0P>
2023-06-05 17:36:29 +0530

    New ClassCreated and added methods

---
+++ StockAccounting_ERP/src/test/java/commonFunctions/Functions_Library.java
@@ -0,0 +1,22 @@
+package commonFunctions;
+
+public class Functions_Library {
+public void add()
+{
+	int a=4556,b=987,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void division()
+{
+	int a=543,b=5,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void mul()
+{
+	int a=456,b=7,c;
+	c=a+b;
+	System.out.println(c);
+}
+}
